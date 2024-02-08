40f6383a5d02d07c58b0843a4d9ea203f10708c2
GenaPlem <shainkill1337@gmail.com>
2024-02-08 14:07:03 +0000

    Add QuoteTweet component

---
+++ src/components/QuoteTweet.js
@@ -0,0 +1,6 @@
+export const QuoteTweet = () => {
+    return (<span>
+            <a href="twitter.com/intent/tweet" target="_blank" className="quotes-display__tweet">TweetIt</a>
+        </span>
+    )
+}
