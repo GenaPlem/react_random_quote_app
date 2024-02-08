fc82e9fdd662ba8fe3c3f3ca46e95e2666fceae9
GenaPlem <shainkill1337@gmail.com>
2024-02-08 17:18:04 +0000

    Add function to get data from API

--- src/App.js
+++ src/App.js
@@ -9,12 +9,31 @@ import {useEffect, useState} from "react";
 function App() {
     const [quote, setQuote] = useState({})
 
+    const fetchQuote = async () => {
+        let data
+        try {
+            const response = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+            data = await response.json();
+            if (data && data.length > 0) {
+                console.log(data)
+                setQuote(data[0]);
+            }
+        } catch (error) {
+            console.error("Error to fetch data:", error);
+        }
+        return data
+    }
+
+    useEffect(() => {
+        fetchQuote();
+    }, [])
+
     return (
         <div className="quotes-display" id="quote-box">
-            <QuoteDisplay/>
+            <QuoteDisplay quote={quote.quote} author={quote.author}/>
             <div className="quotes-display__actions">
                 <QuoteTweet/>
-                <QuoteButton/>
+                <QuoteButton fetchQuote={fetchQuote}/>
             </div>
         </div>
     );
