b4ad6931d3f768bcb55a63b53bb4e496b6d1f5c8
GenaPlem <shainkill1337@gmail.com>
2024-02-08 17:16:39 +0000

    Add state to store quotes in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
+
 import {QuoteDisplay} from "./components/QuoteDisplay";
 import {QuoteButton} from "./components/QuoteButton";
 import {QuoteTweet} from "./components/QuoteTweet";
 
+import {useEffect, useState} from "react";
+
 function App() {
+    const [quote, setQuote] = useState({})
+
     return (
         <div className="quotes-display" id="quote-box">
             <QuoteDisplay/>
