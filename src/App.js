cc4daeb9af00c75bf631b38369eb03a6258b7297
GenaPlem <shainkill1337@gmail.com>
2024-02-09 11:58:27 +0000

    Refactor App to class-based to avoid StrictMode double mount

--- src/App.js
+++ src/App.js
@@ -4,39 +4,47 @@ import {QuoteDisplay} from "./components/QuoteDisplay";
 import {QuoteButton} from "./components/QuoteButton";
 import {QuoteTweet} from "./components/QuoteTweet";
 
-import {useEffect, useState} from "react";
+import {Component} from "react";
 
-function App() {
-    const [quote, setQuote] = useState({})
-
-    const fetchQuote = async () => {
-        let data
-        try {
-            const response = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
-            data = await response.json();
-            if (data && data.length > 0) {
-
-                setQuote(data[0]);
-            }
-        } catch (error) {
-            console.error("Error to fetch data:", error);
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quote: '',
+            author: ''
         }
-        return data[0]
+        this.fetchQuote = this.fetchQuote.bind(this);
     }
 
-    useEffect(() => {
-        fetchQuote();
-    }, [])
+    fetchQuote() {
+        fetch('https://api.breakingbadquotes.xyz/v1/quotes')
+            .then(data => data.json())
+            .then(data => {
+                if (data && data.length > 0) {
+                    this.setState({
+                            quote: data[0].quote,
+                            author: data[0].author
+                        }
+                    )
+                }
+            });
+    }
 
-    return (
-        <div className="quotes-display" id="quote-box">
-            <QuoteDisplay quote={quote.quote} author={quote.author}/>
-            <div className="quotes-display__actions">
-                <QuoteTweet/>
-                <QuoteButton fetchQuote={fetchQuote}/>
+    componentDidMount() {
+        this.fetchQuote()
+    }
+
+    render() {
+        return (
+            <div className="quotes-display" id="quote-box">
+                <QuoteDisplay quote={this.state.quote} author={this.state.author}/>
+                <div className="quotes-display__actions">
+                    <QuoteTweet/>
+                    <QuoteButton fetchQuote={this.fetchQuote}/>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
