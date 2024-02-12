d6b86820290da724fa432248af5d7f913210395f
GenaPlem <shainkill1337@gmail.com>
2024-02-12 12:59:40 +0000

    Add id's to elements

--- src/components/QuoteDisplay.js
+++ src/components/QuoteDisplay.js
@@ -1,7 +1,7 @@
 export const QuoteDisplay = ({quote, author, isLoading}) => {
     return (
         <div className={`${isLoading ? 'fade-out' : 'fade-in'}`}>
-            <p className="quotes-display__quote" id="quote-text">{quote}</p>
+            <p className="quotes-display__quote" id="text">{quote}</p>
             <p className="quotes-display__author" id="author">- {author}</p>
         </div>
     )
--- src/components/QuoteTweet.js
+++ src/components/QuoteTweet.js
@@ -1,7 +1,7 @@
 export const QuoteTweet = () => {
     return (
         <a href="https://www.twitter.com/intent/tweet" target="_blank" rel="noreferrer"
-           className="quotes-display__tweet">
+           className="quotes-display__tweet" id="tweet-quote">
             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M14.1757 0.867188H16.9357L10.9058 8.49269L18 18.8672H12.4455L8.0955 12.5743L3.117 18.8672H0.3555L6.8055 10.7106L0 0.868017H5.6955L9.62775 6.61992L14.1757 0.867188ZM13.2075 17.0401H14.7368L4.8645 2.5989H3.2235L13.2075 17.0401Z"
